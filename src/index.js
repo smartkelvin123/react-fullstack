d1c4a068dc3dae14f104def7551dbbe7f51aa427
smartkelvin123 <105611054+smartkelvin123@users.noreply.github.com>
2023-04-15 23:45:29 +0100

    added notes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import App from "./App";
+
+import axios from "axios";
+
+// axios.get("http://localhost:3003/notes").then((response) => {
+//   const notes = response.data;
+//   console.log(notes);
+// });
+// const promise2 = axios.get("http://localhost:3001/foobar");
+// console.log(promise2);
+
+const notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true,
+  },
+];
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App notes={notes} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
